a937161bf8df75290ade44ce7f7972c5b7d4f02d
Haiyue Song <shyyhs@sjtu.edu.cn>
2018-11-02 17:37:31 +0900

    gift1

--- usaco/Chapter1/Section1_2/test.cpp
+++ usaco/Chapter1/Section1_2/test.cpp
@@ -1,10 +1,30 @@
 #include<iostream>
-#include<cstdio>
-#include<cstdlib>
+#include<utility>
+#include<string>
+#include<tuple>
+#include<stdexcept>
+#include<map>
 using namespace std;
 
 int main()
 {
-    cout<<1234<<endl;
+    map<char,int> mymap;
+    map<char,int>::iterator it;
+    // insert some values:
+    mymap['a']=10;
+    mymap['b']=20;
+    mymap['c']=30;
+    mymap['d']=40;
+    mymap['e']=50;
+    mymap['f']=60;    
+    mymap.insert(pair<char,int>('g',70));
+    mymap.erase('a');
+    for (it=mymap.begin(); it!=mymap.end(); ++it)
+    {
+        cout<<it->first<<' '<<it->second<<endl;
+    }
+
     return 0;
+
 }
+
