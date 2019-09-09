206cd6e96bb537bb5107548f783be13e878e26d8
shyyhs <songskg6666@gmail.com>
2019-09-09 17:48:20 +0900

    abc140

---
+++ atcoder/ABC140/e.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<set>
+typedef long long ll;
+const ll longinf=1LL<<60;
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
+
